ba3279bcedc106861f77696f61d105911473ef77
solinghol <qweaaq@duck.com>
2023-02-19 23:00:02 +0000

    Bot

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from asyncio import sleep
 import logging
 
 
-class tools:
+class Tools:
     @staticmethod
     def get_audio_url_yt(url):  # get audio info by url from youtube
         YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'False'}
@@ -58,9 +58,9 @@ class tools:
 
     @staticmethod
     def add_server(id1):
-        a = tools.read_json('q.json')
+        a = Tools.read_json('q.json')
         a.update({id1: {'id_or_url': [], 'type': [], 't': [], 'num': 'None', 'name': []}})
-        tools.write_json('q.json', a)
+        Tools.write_json('q.json', a)
 
     @staticmethod
     def exceptions(func):
@@ -68,17 +68,17 @@ class tools:
             try:
                 await func(ctx, *args)
             except KeyError:
-                tools.add_server(ctx.message.guild.id)
+                Tools.add_server(ctx.message.guild.id)
                 await func(ctx, *args)
 
         return inner
 
 
-class vk_tools:
+class VK_Tools:
     @staticmethod
     def get_audio_by_id(t, url1):  # get audio info by id from vk
         req = requests.session()
-        json1 = tools.read_json('config.json')
+        json1 = Tools.read_json('config.json')
         headers = {'User-Agent': json1['user_agent']}
         params = {'access_token': json1['vk_token'], 'owner_id': url1, 'album_id': None, 'audio_ids': None,
                   'need_user': 0, 'offset': t, 'count': 1, 'v': '5.81'}
@@ -88,7 +88,7 @@ class vk_tools:
     @staticmethod
     def get_audio_by_search(url1):  # get audio info by search from vk
         req = requests.session()
-        json1 = tools.read_json('config.json')
+        json1 = Tools.read_json('config.json')
         headers = {'User-Agent': json1['user_agent']}
         params = {'access_token': json1['vk_token'], 'q': url1, 'auto_complete': 0, 'lyrics': 0, 'performer_only': 0,
                   'sort': 2, 'search_own': 0, 'offset': 0, 'count': 5, 'v': '5.81'}
@@ -109,7 +109,7 @@ class vk_tools:
 
     @staticmethod
     def get_count(url1):  # get count of music in account from vk
-        j = vk_tools.get_audio_by_id(0, url1)
+        j = VK_Tools.get_audio_by_id(0, url1)
         return j['response']['count']
 
     @staticmethod
@@ -118,7 +118,7 @@ class vk_tools:
 
     @staticmethod
     def check_user(ctx):
-        a = tools.read_json('q.json')
+        a = Tools.read_json('q.json')
         for i in a.keys():
             for j in a[i].keys():
                 if j == str(ctx.author):
@@ -132,7 +132,7 @@ class queue:
         id1 = str(ctx.message.guild.id)
         mes = ''
         t = 0
-        json1 = tools.read_json('q.json')
+        json1 = Tools.read_json('q.json')
         if not json1[id1]['name']:
             mes = 'None'
         else:
@@ -143,18 +143,18 @@ class queue:
 
     @staticmethod
     def clear():
-        Json = tools.read_json('q.json')
+        Json = Tools.read_json('q.json')
         for i in Json.keys():
             Json[i]['id_or_url'] = []
             Json[i]['type'] = []
             Json[i]['t'] = []
             Json[i]['name'] = []
             Json[i].update(num='None')
-        tools.write_json('q.json', Json)
+        Tools.write_json('q.json', Json)
 
     @staticmethod
     def add_json(id_or_url, type1, t, ctx, name):
-        json1 = tools.read_json('q.json')
+        json1 = Tools.read_json('q.json')
         id1 = str(ctx.message.guild.id)
         if len(json1[id1]['id_or_url']) >= 5:
             queue.clear()
@@ -163,21 +163,21 @@ class queue:
         json1[id1]['t'].append(t)
         json1[id1]['name'].append(name)
         logging.info(f'json1 is: {str(json1)}')
-        tools.write_json('q.json', json1)
+        Tools.write_json('q.json', json1)
 
     @staticmethod
     def delete_json(num, ctx):
-        json1 = tools.read_json('q.json')
+        json1 = Tools.read_json('q.json')
         id1 = str(ctx.message.guild.id)
         json1[id1]['id_or_url'].pop(num)  # delete in queue
         json1[id1]['type'].pop(num)
         json1[id1]['t'].pop(num)
         logging.info(f'json1 is: {str(json1)}')
-        tools.write_json('q.json')
+        Tools.write_json('q.json')
 
     @staticmethod
     def check(num, ctx):
-        json1 = tools.read_json('q.json')
+        json1 = Tools.read_json('q.json')
         if len(json1[str(ctx.message.guild.id)]['id_or_url']) >= 1:
             try:
                 num = int(num)
@@ -201,25 +201,25 @@ class queue:
 
     @staticmethod
     def set_queue_num(num2, ctx):
-        json1 = tools.read_json('q.json')
+        json1 = Tools.read_json('q.json')
         json1[str(ctx.message.guild.id)].update(num=num2)
-        tools.write_json('q.json', json1)
+        Tools.write_json('q.json', json1)
         logging.info(f'Changed num is : {queue.check_queue_num(ctx)}')
 
     @staticmethod
     def check_queue_num(ctx):
-        json1 = tools.read_json('q.json')
+        json1 = Tools.read_json('q.json')
         return json1[str(ctx.message.guild.id)]['num']
 
     @staticmethod
     def check_rm(ctx):
-        json1 = tools.read_json('q.json')
+        json1 = Tools.read_json('q.json')
         id1 = str(ctx.message.guild.id)
         try:
             len(json1[id1])
         except:
-            tools.add_server(id1)
-            json1 = tools.read_json('q.json')
+            Tools.add_server(id1)
+            json1 = Tools.read_json('q.json')
         logging.info(f'q.json is: {json1}')
         if len(json1[id1]['id_or_url']) >= 5:
             queue.clear()
@@ -227,7 +227,7 @@ class queue:
 
 # start
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
-json1 = tools.read_json('config.json')  # open config file
+json1 = Tools.read_json('config.json')  # open config file
 logging.info('config.json is open')
 queue.clear()
 client = commands.Bot(command_prefix=json1["prefix"])
@@ -244,7 +244,7 @@ async def pause(ctx):  # for pause music
     if voice_client and voice_client.is_playing():
         try:
             await voice_client.pause()  # pause
-        except:
+        except TypeError:
             logging.info('Audio is paused by ' + str(ctx.author))
     else:
         logging.info('Nothing is played')
@@ -256,8 +256,7 @@ async def resume(ctx):  # for resume music
     if not voice_client.is_playing():
         try:
             await voice_client.resume()  # resume
-        except Exception as e:
-            print(e)
+        except TypeError:
             logging.info(f'Audio is resumed by {ctx.author}')
     else:
         logging.info('Nothing is played')
@@ -266,11 +265,11 @@ async def resume(ctx):  # for resume music
 @client.command(name='ypu')
 async def youtubeplay_url(ctx, url1: str):
     queue.check_rm(ctx)
-    tools.start_logging(ctx, url1, type1='yt')
+    Tools.start_logging(ctx, url1, type1='yt')
     # loging block
     voice = get(client.voice_clients, guild=ctx.guild)
     logging.info('None queue')
-    i, s, d, k = tools.get_audio_url_yt(url1)  # get audio source
+    i, s, d, k = Tools.get_audio_url_yt(url1)  # get audio source
     if await qucheck(ctx, voice, 'yp', url1, 'search', s):
         return 0
     logging.info(f"Now playing: {s}, Duration is: {d}")
@@ -279,8 +278,8 @@ async def youtubeplay_url(ctx, url1: str):
         await voice.move_to(channel)  # connect voice channel
     else:
         voice = await channel.connect()
-    tools.download_png(k)
-    msg = tools.set_msg(s, tools.set_time(d))
+    Tools.download_png(k)
+    msg = Tools.set_msg(s, Tools.set_time(d))
     await ctx.send(msg, file=discord.File('cover.png'))  # send info msg
     logging.info(f'Url is: {i}')
     voice.play(FFmpegOpusAudio(source=i,
@@ -298,18 +297,18 @@ async def vkll(ctx, url1: str, t):
             type1 = 'id'
         else:
             type1 = 'search'
-        tools.start_logging(ctx, url1, type1)
+        Tools.start_logging(ctx, url1, type1)
         # loging block
         voice = get(client.voice_clients, guild=ctx.guild)
         logging.info('None queue')
         if t == 'search':
-            j = vk_tools.get_audio_by_search(url1.replace('_', ''))  # get audio source
+            j = VK_Tools.get_audio_by_search(url1.replace('_', ''))  # get audio source
         else:
-            j = vk_tools.get_audio_by_id(t, url1.replace('_', ''))
+            j = VK_Tools.get_audio_by_id(t, url1.replace('_', ''))
         logging.info(f'Pack is: {j}')
-        title, dur = vk_tools.format_tool(vk_tools.get_title(j), tools.set_time(vk_tools.get_dur(j)))  # format info
+        title, dur = VK_Tools.format_tool(VK_Tools.get_title(j), Tools.set_time(VK_Tools.get_dur(j)))  # format info
         logging.info(f'Title is: {title}, Duration is: {dur}')
-        url = vk_tools.get_url(j)  # get audio url
+        url = VK_Tools.get_url(j)  # get audio url
         if await qucheck(ctx, voice, type1, url1, t, title):
             return 0
         channel = ctx.message.author.voice.channel
@@ -317,7 +316,7 @@ async def vkll(ctx, url1: str, t):
             await voice.move_to(channel)  # connect voice channel
         else:
             voice = await channel.connect()
-        await ctx.send(tools.set_msg(title, dur))  # send info
+        await ctx.send(Tools.set_msg(title, dur))  # send info
         logging.info(f'Url is: {url}')
         voice.play(FFmpegOpusAudio(source=url))  # play audio
         while voice.is_playing() or voice.is_paused():
@@ -342,12 +341,12 @@ async def qucheck(ctx, voice, type1, url1, t, name):
 
 @client.command()
 async def vk(ctx, url1):
-    if not vk_tools.check_user(ctx):
+    if not VK_Tools.check_user(ctx):
         ctx.reply('Please set vk id. Use ">set" for it')
         return 0
     voice = get(client.voice_clients, guild=ctx.guild)
     t = 0
-    for j in range(vk_tools.get_count(url1)):  # get count of audio in account
+    for j in range(VK_Tools.get_count(url1)):  # get count of audio in account
         await pause(ctx)
         await vkll(ctx, url1, t)
         while voice and voice.is_playing() or voice and voice.is_paused():
@@ -356,23 +355,23 @@ async def vk(ctx, url1):
 
 
 @client.command(name='setid')
-@tools.exceptions
+@Tools.exceptions
 async def set_vk(ctx, url1):
     author1 = str(ctx.author)
     f = {author1: url1}
-    a = tools.read_json('q.json')
+    a = Tools.read_json('q.json')
     if url1.isdigit():
         a[str(ctx.message.guild.id)].update(f)
         await ctx.send('Your id has been writed')
-        tools.write_json('q.json', a)
+        Tools.write_json('q.json', a)
     else:
         await ctx.send('''Did not work(
-    Check if the id is entered correctly''')
+Check if the id is entered correctly''')
 
 
 @client.command()
 async def ss(ctx, url1):
-    a = tools.read_json('q.json')
+    a = Tools.read_json('q.json')
     url = a[str(ctx.message.guild.id)][str(ctx.author)]
     await vkll(ctx, url, url1)
 
@@ -404,13 +403,9 @@ async def start_queue(ctx):
 
 
 @client.command(name='n')
-@tools.exceptions
+@Tools.exceptions
 async def next(ctx):
-    if len(tools.read_json('q.json')[str(ctx.message.guild.id)]['id_or_url']) < 1:
-        logging.info('no Next today')
-        await ctx.send('no Next today')
-        return 0
-    else:
+    try:
         logging.info('Next queue')
         d = 1
         num = queue.check_queue_num(ctx)
@@ -432,12 +427,16 @@ async def next(ctx):
             if type1 == 'id':
                 queue.set_queue_num(num + d, ctx)
                 await vkll(ctx, id_or_url, t)
+    except IndexError:
+        await resume(ctx)
+        logging.info('no Next today')
+        await ctx.send('no Next today')
 
 
 @client.command(name='p')
 async def prev(ctx):
-    if tools.read_json('q.json')[str(ctx.message.guild.id)]['num'] == 'None' or \
-            tools.read_json('q.json')[str(ctx.message.guild.id)]['num'] == 0:
+    if Tools.read_json('q.json')[str(ctx.message.guild.id)]['num'] == 'None' or \
+            Tools.read_json('q.json')[str(ctx.message.guild.id)]['num'] == 0:
         logging.info('no Prev today')
         await ctx.reply('no Prev today')
         return 0
